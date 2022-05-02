86d04759c1f6133e799f6b0917710725e5e8b88b
Anna Vi <nibiruru@yandex.ru>
2022-05-02 23:24:25 +0300

    feat: complete stack

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor() {
+    this.list = [];
+  }
+
+  push(element) {
+   this.list.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let deleted = this.list[this.list.length-1]
+    this.list.pop();
+    return deleted;
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.list[this.list.length-1];
   }
 }
 
